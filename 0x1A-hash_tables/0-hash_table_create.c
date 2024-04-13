2ece2fbfba19919bfbfe7400412e6f9ca27d5a15
root <root@kirolos>
2024-04-13 04:16:40 +0200

    f

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
 
+
 /**
  * hash_table_create - creates a hash table
  * @size: size of the array
@@ -14,14 +15,14 @@ hash_table_t *hash_table_create(unsigned long int size)
     /* Allocate memory for the hash table structure */
     hash_table = malloc(sizeof(hash_table_t));
     if (hash_table == NULL)
-        return NULL;
+        return (NULL);
 
     /* Allocate memory for the array */
     hash_table->array = malloc(sizeof(hash_node_t *) * size);
     if (hash_table->array == NULL)
     {
         free(hash_table);
-        return NULL;
+        return (NULL);
     }
 
     /* Initialize all elements of the array to NULL */
@@ -30,4 +31,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 
     hash_table->size = size;
 
-    return hash_table;}
+    return (hash_table);
+}
+
