c7eab719085ce626e6ee07b38855dcd460f1d794
root <root@kirolos>
2024-04-13 04:33:15 +0200

    f

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,61 +1,27 @@
 #include "hash_tables.h"
-#include <stdlib.h>
 
 /**
  * hash_table_create - creates a hash table
  * @size: size of the array
  *
- * Return: pointer to the newly created hash table, or NULL if fails
+ * Return: pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table = NULL;
+	hash_table_t *hash_table;
+	unsigned long int i;
 
-    /* Allocate memory for the hash table structure */
-    hash_table = calloc(1, sizeof(hash_table_t));
-    if (hash_table == NULL)
-        return NULL;
-
-    /* Allocate memory for the array */
-    hash_table->array = calloc(size, sizeof(hash_node_t *));
-    if (hash_table->array == NULL)
-    {
-        free(hash_table);
-        return NULL;
-    }
-
-    hash_table->size = size;
-
-    return hash_table;
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
+	return (hash_table);
 }
-#include "hash_tables.h"
-#include <stdlib.h>
-
-/**
- * hash_table_create - creates a hash table
- * @size: size of the array
- *
- * Return: pointer to the newly created hash table, or NULL if fails
- */
-hash_table_t *hash_table_create(unsigned long int size)
-{
-    hash_table_t *hash_table = NULL;
-
-    /* Allocate memory for the hash table structure */
-    hash_table = calloc(1, sizeof(hash_table_t));
-    if (hash_table == NULL)
-        return NULL;
-
-    /* Allocate memory for the array */
-    hash_table->array = calloc(size, sizeof(hash_node_t *));
-    if (hash_table->array == NULL)
-    {
-        free(hash_table);
-        return NULL;
-    }
-
-    hash_table->size = size;
-
-    return hash_table;
-}
-
