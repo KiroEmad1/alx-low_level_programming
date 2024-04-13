0d7e068ffb2c64ec7c588613b07eee3bf922319f
root <root@kirolos>
2024-04-13 04:22:27 +0200

    f

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,5 @@
 #include "hash_tables.h"
-
+#include <stdlib.h>
 
 /**
  * hash_table_create - creates a hash table
@@ -10,27 +10,52 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *hash_table = NULL;
-    unsigned long int i;
 
     /* Allocate memory for the hash table structure */
-    hash_table = malloc(sizeof(hash_table_t));
+    hash_table = calloc(1, sizeof(hash_table_t));
     if (hash_table == NULL)
-        return (NULL);
+        return NULL;
 
     /* Allocate memory for the array */
-    hash_table->array = malloc(sizeof(hash_node_t *) * size);
+    hash_table->array = calloc(size, sizeof(hash_node_t *));
     if (hash_table->array == NULL)
     {
         free(hash_table);
-        return (NULL);
+        return NULL;
     }
-
-    /* Initialize all elements of the array to NULL */
-    for (i = 0; i < size; i++)
-        hash_table->array[i] = NULL;
 
     hash_table->size = size;
 
-    return (hash_table);
+    return hash_table;
+}
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to the newly created hash table, or NULL if fails
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *hash_table = NULL;
+
+    /* Allocate memory for the hash table structure */
+    hash_table = calloc(1, sizeof(hash_table_t));
+    if (hash_table == NULL)
+        return NULL;
+
+    /* Allocate memory for the array */
+    hash_table->array = calloc(size, sizeof(hash_node_t *));
+    if (hash_table->array == NULL)
+    {
+        free(hash_table);
+        return NULL;
+    }
+
+    hash_table->size = size;
+
+    return hash_table;
 }
 
